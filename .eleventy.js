01852fc8b4fe395fec3172a00a640ef0d4e026af
Dan Leatherman <dleatherman@gmail.com>
2023-02-24 14:28:07 -0500

    packages

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const CleanCSS = require("clean-css");
+const { DateTime, Duration } = require("luxon");
+const htmlmin = require("html-minifier");
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+
+  // Minify CSS
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // Minify HTML output
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath.indexOf(".html") > -1) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data",
+      includes: "_includes"
+    }
+  }
+
+};
