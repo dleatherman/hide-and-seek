c044d970ee79ae5f90a5e9284460f9a4941b5aa1
Dan Leatherman <dleatherman@gmail.com>
2023-02-27 15:47:30 -0500

    add serviceworker back in

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,16 +1,16 @@
 
-self.addEventListener("install", async event => {
-  const cache = await caches.open("pwa-assets");
-  // it stores all resources on first SW install
-  cache.addAll(["/"]);
-});
+// self.addEventListener("install", async event => {
+//   const cache = await caches.open("pwa-assets");
+//   // it stores all resources on first SW install
+//   cache.addAll(["/"]);
+// });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      // It can update the cache to serve updated content on the next request
-      return cachedResponse || fetch(event.request);
-    }
-    )
-  )
-});
+// self.addEventListener("fetch", event => {
+//   event.respondWith(
+//     caches.match(event.request).then(cachedResponse => {
+//       // It can update the cache to serve updated content on the next request
+//       return cachedResponse || fetch(event.request);
+//     }
+//     )
+//   )
+// });
